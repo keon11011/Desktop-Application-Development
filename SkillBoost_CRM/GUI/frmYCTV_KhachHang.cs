e564a2ab76c6b823f4b3503d8fd7d1f5a7f1cfcb
Vĩ Bùi <vibuixuan2812@gmail.com>
2023-12-17 23:59:21 +0700

    Merge file YCTV_KhachHang

---
+++ SkillBoost_CRM/GUI/frmYCTV_KhachHang.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class frmYCTV_KhachHang : Form
+    {
+        public frmYCTV_KhachHang()
+        {
+            InitializeComponent();
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmYCTV_KhachHang_Load(object sender, EventArgs e)
+        {
+            string[] arr = { "IT Business Analysis", "Product Owner Master Class", "SQL Advanced Class", "Data Analysis", "Business Intelligence", "Excel Advanced Class", "Database Foundation Class", "Desktop Coding Class", "Python Advance Coding Class", "Business Data Engineer " };
+            checkedListBox1.Items.AddRange(arr);
+        }
+    }
+}
