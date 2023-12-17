aaf4186f6f8b67c06993f0534e24f3ea3cfee595
Vĩ Bùi <vibuixuan2812@gmail.com>
2023-12-17 21:31:36 +0700

    Re-add YCTV

---
+++ SkillBoost_CRM/BUS/BUS_YeuCauTuVan.cs
@@ -0,0 +1,30 @@
+﻿using DAL;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class BUS_YeuCauTuVan
+    {
+        DAL_YeuCauTuVan dAL_YeuCauTuVan = new DAL_YeuCauTuVan();
+        public DataTable SelectYCTV()
+        {
+            return dAL_YeuCauTuVan.SelectYCTV();
+        }
+        public DataTable TimYCTV(string tenYCTV)
+        {
+            return dAL_YeuCauTuVan.TimYCTV(tenYCTV);
+        }
+        public DataTable LocYCTV(string trangthailocYCTV)
+        {
+            return dAL_YeuCauTuVan.LocYCTV(trangthailocYCTV);
+        }
+
+    }
+
+}
