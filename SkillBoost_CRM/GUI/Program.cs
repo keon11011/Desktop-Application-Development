042ec3cc97adc9eae5d8a8371716afdec8d336c7
Vĩ Bùi <vibuixuan2812@gmail.com>
2023-12-17 21:31:36 +0700

    Re-add YCTV

--- SkillBoost_CRM/GUI/Program.cs
+++ SkillBoost_CRM/GUI/Program.cs
@@ -16,8 +16,8 @@ namespace GUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new frmDangNhap());
-            Application.Run(new frmDanhSachQuyDinhGiamGia());
+            Application.Run(new frmYeuCauTuVan());
+            
         }
     }
 }
