7c3e030d5f13155af2f96aee9a98428cf2505f63
DecimLam <thynhalam919@gmail.com>
2023-12-17 17:50:06 +0700

    Form Danh Sách QuyDinhGiamGia và Form Tạo QuyDinhGiamGia

---
+++ SkillBoost_CRM/DTO/DTO_QuyDinhGiamGia.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class DTO_QuyDinhGiamGia
+    {
+        private string _maQuyDinhGiamGia;
+        private string _moTaLoaiGiamGia;
+        private int _soLuongKhoaHocDangKy;
+        private string _maNgheNghiep;
+        private string _tenNgheNghiep;
+        private DateTime _ngayBatDau;
+        private DateTime _ngayKetThuc;
+        private int _phanTramGiamGiaMacDinh;
+        private int _phanTramGiamGiaToiDa;
+        private DateTime _taoVaoLuc;
+        private string _taoBoi;
+        private DateTime _chinhSuaLanCuoiVaoLuc;
+        private string _chinhSuaLanCuoiBoi;
+
+        public string MaQuyDinhGiamGia { get => _maQuyDinhGiamGia; set => _maQuyDinhGiamGia = value; }
+        public string MoTaLoaiGiamGia { get => _moTaLoaiGiamGia; set => _moTaLoaiGiamGia = value; }
+        public int SoLuongKhoaHocDangKy { get => _soLuongKhoaHocDangKy; set => _soLuongKhoaHocDangKy = value; }
+        public string MaNgheNghiep { get => _maNgheNghiep; set => _maNgheNghiep = value; }
+        public string TenNgheNghiep { get => _tenNgheNghiep; set => _tenNgheNghiep = value; }
+        public DateTime NgayBatDau { get => _ngayBatDau; set => _ngayBatDau = value; }
+        public DateTime NgayKetThuc { get => _ngayKetThuc; set => _ngayKetThuc = value; }
+        public int PhanTramGiamGiaMacDinh { get => _phanTramGiamGiaMacDinh; set => _phanTramGiamGiaMacDinh = value; }
+        public int PhanTramGiamGiaToiDa { get => _phanTramGiamGiaToiDa; set => _phanTramGiamGiaToiDa = value; }
+        public DateTime TaoVaoLuc { get => _taoVaoLuc; set => _taoVaoLuc = value; }
+        public string TaoBoi { get => _taoBoi; set => _taoBoi = value; }
+        public DateTime ChinhSuaLanCuoiVaoLuc { get => _chinhSuaLanCuoiVaoLuc; set => _chinhSuaLanCuoiVaoLuc = value; }
+        public string ChinhSuaLanCuoiBoi { get => _chinhSuaLanCuoiBoi; set => _chinhSuaLanCuoiBoi = value; }
+
+        public DTO_QuyDinhGiamGia()
+        {
+
+        }
+        public DTO_QuyDinhGiamGia(string maQuyDinhGiamGia, string moTaLoaiGiamGia, int soLuongKhoaHocDangKy, string maNgheNghiep, string tenNgheNghiep, DateTime ngayBatDau, DateTime ngayKetThuc, int phanTramGiamGiaMacDinh, int phanTramGiamGiaToiDa, DateTime taoVaoLuc, string taoBoi, DateTime chinhSuaLanCuoiVaoLuc, string chinhSuaLanCuoiBoi)
+        {
+            MaQuyDinhGiamGia = maQuyDinhGiamGia;
+            MoTaLoaiGiamGia = moTaLoaiGiamGia;
+            SoLuongKhoaHocDangKy = soLuongKhoaHocDangKy;
+            MaNgheNghiep = maNgheNghiep;
+            TenNgheNghiep = tenNgheNghiep;
+            NgayBatDau = ngayBatDau;
+            NgayKetThuc = ngayKetThuc;
+            PhanTramGiamGiaMacDinh = phanTramGiamGiaMacDinh;
+            PhanTramGiamGiaToiDa = phanTramGiamGiaToiDa;
+            TaoVaoLuc = taoVaoLuc;
+            TaoBoi = taoBoi;
+            ChinhSuaLanCuoiVaoLuc = chinhSuaLanCuoiVaoLuc;
+            ChinhSuaLanCuoiBoi = chinhSuaLanCuoiBoi;
+        }
+    }
+}
---
+++ SkillBoost_CRM/GUI/frmDanhSachQuyDinhGiamGia.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace GUI
+{
+    partial class frmDanhSachQuyDinhGiamGia
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbDSachQuyDinhGiamGia = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnTaoQuyDinhGiamGia = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbDSachQuyDinhGiamGia
+            // 
+            this.lbDSachQuyDinhGiamGia.AutoSize = true;
+            this.lbDSachQuyDinhGiamGia.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDSachQuyDinhGiamGia.Location = new System.Drawing.Point(12, 9);
+            this.lbDSachQuyDinhGiamGia.Name = "lbDSachQuyDinhGiamGia";
+            this.lbDSachQuyDinhGiamGia.Size = new System.Drawing.Size(346, 29);
+            this.lbDSachQuyDinhGiamGia.TabIndex = 0;
+            this.lbDSachQuyDinhGiamGia.Text = "Danh sách quy định giảm giá";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(17, 59);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(960, 482);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnTaoQuyDinhGiamGia
+            // 
+            this.btnTaoQuyDinhGiamGia.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnTaoQuyDinhGiamGia.Location = new System.Drawing.Point(857, 16);
+            this.btnTaoQuyDinhGiamGia.Name = "btnTaoQuyDinhGiamGia";
+            this.btnTaoQuyDinhGiamGia.Size = new System.Drawing.Size(75, 23);
+            this.btnTaoQuyDinhGiamGia.TabIndex = 2;
+            this.btnTaoQuyDinhGiamGia.Text = "Tạo";
+            this.btnTaoQuyDinhGiamGia.UseVisualStyleBackColor = true;
+            this.btnTaoQuyDinhGiamGia.Click += new System.EventHandler(this.btnTaoQuyDinhGiamGia_Click);
+            // 
+            // frmDanhSachQuyDinhGiamGia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(989, 553);
+            this.Controls.Add(this.btnTaoQuyDinhGiamGia);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lbDSachQuyDinhGiamGia);
+            this.Name = "frmDanhSachQuyDinhGiamGia";
+            this.Text = "frmDanhSachQuyDinhGiamGia";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbDSachQuyDinhGiamGia;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnTaoQuyDinhGiamGia;
+    }
+}
